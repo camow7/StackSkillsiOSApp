adb847ea83fea7a3caf7def62d5d13916f1f6bcb
Cameron Owen <cameron.owen@live.com.au>
2017-01-22 14:25:45 +1100

    My first Stack Skills Commit

--- StackSkillsHelloWorld/ViewController.swift
+++ StackSkillsHelloWorld/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            theLabel.text = "Tapped more than 10 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        theLabel.text = "Hello World"
     }
 
     override func didReceiveMemoryWarning() {
