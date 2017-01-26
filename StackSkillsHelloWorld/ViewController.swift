e616471143a09ec4d1ca131497c77cdf117f6fae
Cameron Owen <cameron.owen@live.com.au>
2017-01-26 12:21:34 +1100

    Added text fields to take in user input

--- StackSkillsHelloWorld/ViewController.swift
+++ StackSkillsHelloWorld/ViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var theLabel: UILabel!
-    
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonPressed(_ sender: Any) {
-        tapCount = tapCount + 1
-        if tapCount >= 5 {
-            theLabel.text = "Tapped more than 5 times!"
-        }
+        print(text1.text!)
+        print(text2.text!)
     }
     
     override func viewDidLoad() {
